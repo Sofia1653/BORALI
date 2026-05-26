08cf6794a315d0b9daeb51c7c08a6a231a8781b0
Beatriz Costa <beatriz.costa@liferay.com>
2026-05-26 18:57:16 -0300

    Remoção do pacote repositories legado

--- backend/borali/src/main/java/com/cesar/borali/repositories/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cesar.borali.repositories;
-
-import com.cesar.borali.models.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- backend/borali/src/main/java/com/cesar/borali/repositories/LocalizacaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cesar.borali.repositories;
-
-import com.cesar.borali.models.Localizacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocalizacaoRepository extends JpaRepository<Localizacao, Long> {
-}
