14161cf45b36b9124417dcdfb4fdb1ea22940b0a
Sofia1653 <sofedu2005@gmail.com>
2026-06-17 10:16:04 -0300

    fix: ajustes nas rotas

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,26 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'agenda',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'criar-evento',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'mapa',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'perfil',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
