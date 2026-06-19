740732eaedbaf9ed94e54270d8a10a002bfff9f5
gabsvelozo <gabsmastellari@gmail.com>
2026-06-18 21:57:58 -0300

    fix: correct environment import path in services

--- frontend/src/app/services/agenda.service.ts
+++ frontend/src/app/services/agenda.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, map, switchMap, tap } from 'rxjs';
 import { EventoResponse } from '../models/models';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
